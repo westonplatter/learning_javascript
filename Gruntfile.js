d4ec71440ab633ae81d68e5338b628714c03edfe
Weston Platter <westonplatter@gmail.com>
2013-11-02 21:25:40 -0600

    add grunt file and travis ci file

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'), 
+    jasmine: {
+      specs: "./resources/**/*.js" 
+    },
+  
+    watch: {
+      scripts: {
+        files: "./resources/**/*.js", 
+        tasks: ["jasmine"], 
+        options: {
+          interrupt: true
+        },
+      },
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-jasmine', {});  
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  grunt.registerTask('default', ["jasmine"]);
+};
+
+
