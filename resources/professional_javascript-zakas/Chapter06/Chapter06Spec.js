a7ac120a454bd12b41ecd72091c160e6b79c9170
Weston Platter <westonplatter@gmail.com>
2013-11-07 09:30:33 -0700

    [professional javascript] ch6 - setup basic structure

---
+++ resources/professional_javascript-zakas/Chapter06/Chapter06Spec.js
@@ -0,0 +1,10 @@
+// chapter 6 tests
+
+describe("Chapter 6 Specs - Object Oriented Programming", function(){
+  
+  // we need 1 test to make the Jasmine test suite to run.
+  it("pages 173-216", function() {
+    expect(true).toBe(true);
+  });
+    
+});
