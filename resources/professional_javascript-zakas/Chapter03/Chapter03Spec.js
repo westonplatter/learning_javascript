796af9a9305f0e2956d3114966bfd4f4ecf93ff8
Weston Platter <westonplatter@gmail.com>
2013-11-08 23:25:43 -0700

    [professional javascript] ch3 added test for: - multiply - divide - conditional/ternary operators - if statement - while statement - do while statement - continue - break - with - switch - function args + overload

--- resources/professional_javascript-zakas/Chapter03/Chapter03Spec.js
+++ resources/professional_javascript-zakas/Chapter03/Chapter03Spec.js
@@ -160,67 +160,271 @@ describe("Chapter 3 Specs", function(){
     });
   });
   
-  describe("Operators", function() {
+  describe("page 44 - Operators", function() {
     
     describe("Unary operators", function() {
-      it("page 46: ++ should increment by 1", function() {
+      it("++ should increment by 1", function() {
         var i = 0;
         i++;
         expect(i).toBe(1);
       });
-      
-      it("page 46: -- should decrement by 1", function() {
+      it("should decrement by 1", function() {
         var i = 0;
         i--;
         expect(i).toBe(-1);
       });
-      xit("page 47: ++ increment before", function() {});
-      xit("page 47: ++ increment after", function() {});
+      it("should ++ increment by 1 before evaluation", function(){
+        var i = 0;
+        var result = 2 - ++i;
+        expect(i).toBe(1);
+        expect(result).toBe(1);
+      });
+      it("should ++ increment by 1 after evaluation", function(){
+        var i = 0;
+        var result = 2 - i++;
+        expect(i).toBe(1);
+        expect(result).toBe(2);
+      });
     });
     
-    describe("Bitwise operators", function() {
-    });
+    describe("Bitwise operators", function() {});
     
     describe("Boolean operators", function() {
     });
     
     describe("Multiplicative operators", function() {
+      describe("multiply", function(){
+        it("simple multiplication works as expected", function() {
+          var result = 2 * 3;
+          expect(result).toBe(6);
+        });
+        it("if any number is NaN the result is NaN", function() {
+          var result = 2 * NaN;
+          expect(isNaN(result)).toBe(true);
+        });
+        it("if 0 and Infinity the result is NaN", function() {
+          var result = 0 * NaN;
+          expect(isNaN(result)).toBe(true);
+        });
+        it("if all numbers are not zero and Infinity is 1 of the numbers, the result is NaN", function() {
+          var result = 2 * Infinity;
+          expect(result).toBe(Infinity);
+          
+          var negativeResult = result * -1;
+          expect(negativeResult).toBe(-Infinity);
+        });
+        it("Infinity times Infinity results in Infinity", function() {
+          var result = Infinity * Infinity;
+          expect(result).toBe(Infinity);
+        });
+        it("automatically converts non-Number types to Number via Number() before evaluation", function() {
+          var result = "2" * 3;
+          expect(result).toBe(6);
+        });
+      });
+      describe("divide", function(){
+        it("simple division works as expected", function() {
+          var result = 10/2;
+          expect(result).toBe(5);
+        });
+        it("if either number is NaN, the result is NaN", function() {
+          var result = NaN/2;
+          expect(isNaN(result)).toBe(true);
+        });
+        it("if 0 divided by zero, the result is NaN", function() {
+          var result = 0/0;
+          expect(isNaN(result)).toBe(true);
+        });
+        it("if numerator is non-zero and denominator is 0, result is [-|+]Infinity", function() {
+          var result = -2/0;
+          expect(result).toBe(-Infinity);
+        });
+        it("if Infinity is divided by any number, result [-|+]Infinity", function() {
+          var result = -Infinity/0;
+          expect(result).toBe(-Infinity);
+        });
+        it("automatically converts non-Number types to Number via Number() before evaluation", function() {
+          var result = "10" / 2;
+          expect(result).toBe(5);
+        });
+      }); 
+      describe("modulus", function(){
+        // TODO add tests for thses, pages 60-61
+      });
     });
     
     describe("Additive operators", function() {
+      // TODO add tests for these, pages 61-62
     });
     
     describe("Equality operators", function() {
     });
     
-    describe("Conditional or ternary operators", function() {
-      xit("should how it's the same as if ... else", function() {});
+    describe("Rational operators", function() {
+      // TODO add tests for these, pages 63-65
     });
     
-    describe("If statement", function() {});
+    describe("Conditional or Ternary operators", function() {
+      it("execute if else logic in 1 line", function() {
+        var yes = true;
+        var result = yes ? "yes was true" : "yes was false";
+        expect(result).toBe("yes was true");
+        
+        yes = false;
+        result = yes ? "yes was true" : "yes was false";
+        expect(result).toBe("yes was false");
+      });
+    });
     
-    describe("Do-While statement", function() {});
+    describe("Assignment operators", function() {
+    });
     
-    describe("While statement", function() {});
+    describe("If statement", function() {
+      it("simple if ... else example", function() {
+        var result = "star";
+        if (true){
+          result += " wars";
+        }
+        expect(result).toBe("star wars");    
+        
+        if(false){
+          result += "was a great plot";
+        } else {
+          result += " eposide 1 had no character developlent";
+        }
+        expect(result).toBe("star wars eposide 1 had no character developlent");
+      });
+    });
     
-    describe("For statement", function() {});
+    describe("Do-While statement", function() {
+      it("goes until while logic returns false", function() {
+        var i = 0;
+        do {
+          i++;
+        }while(i < 5);
+        
+        expect(i).toBe(5);
+      });
+      
+      it("is a post-test loop", function() {
+        var i = 0;
+        do {
+          i++;
+        }while(false);
+        expect(i).toBe(1);
+      });
+    });
     
-    describe("For-In statement", function() {});
+    describe("While statement", function() {
+      it("goes until while logic returns false", function() {
+        var i = 0;
+        while (i < 5) {
+          i++;
+        };
+        expect(i).toBe(5);
+      });
+      
+      it("is a pre-test loop", function() {
+        var i = 0;
+        while(false){
+          i++;
+        };
+        expect(i).toBe(0);
+      });
+    });
     
-    describe("Break in loop functions", function() {});
+    describe("For statement", function() {
+      // TODO add tests for thses, pages 70-71
+    });
     
-    describe("Conintue in loop functions", function() {});
+    describe("For-In statement", function() {
+      // TODO add tests for thses, pages 71-72
+    });
     
-    describe("Breaks AND Conintues in loop functions", function() {});
+    describe("Break", function() {
+      it("exits to loop function immediately", function() {
+        var count = 0;
+        for(var p = 0; p < 5; p ++){
+          if (p == 3){
+            break;
+          }
+          count ++;
+        }
+        expect(count).toBe(3);
+      });
+    });
     
-    describe("With statement", function() {});
+    describe("Conintue", function() {
+      it("exits to loop function immediately", function() {
+        var count = 0;
+        for(var p = 0; p < 5; p ++){
+          if (p == 3){
+            continue;
+          }
+          count ++;
+        }
+        expect(count).toBe(4);
+      });
+    });
     
-    describe("Switch statement", function() {});
+    describe("With statement", function() {
+      it("evalates logic on top of with(object)", function() {
+        var hash = {
+          a: "alpha", 
+        };
+        
+        with(hash){
+          var x = a;
+        }
+        
+        expect(x).toBe("alpha");
+      });
+    });
+    
+    describe("Switch statement", function() {
+      var switchFunction = function(value){
+        switch(value){
+          case 1: return "one";
+          case "two": return 2;
+          default: return "default";
+        }
+      };
+      
+      expect(switchFunction(1)).toBe("one");
+      expect(switchFunction("two")).toBe(2);
+      expect(switchFunction()).toBe("default");
+    });
     
     describe("Functions", function(){
-      describe("argments", function() {});
-      describe("overloading", function() {});
+      describe("argments", function() {
+        it("present whether or not declared in method signature", function() {
+          var returnFirstArgument = function(){
+            return arguments[0];
+          };
+          
+          expect(returnFirstArgument(1)).toBe(1);
+        });
+        
+        it("are flexible", function() {
+          var howManyArguments = function(){
+            return arguments.length;
+          };
+          expect( howManyArguments()).toBe(0);
+          expect( howManyArguments("abc", "xyz")).toBe(2);
+        });
+      });
+      
+      describe("overloading", function() {
+        var robot = function(num1, num2){
+          return num1 + num2;
+        };
+        var robot = function(num1){
+          return num1;
+        }
+        
+        expect(robot(1, 2)).toBe(1);
+      });
     });
-    
   });
+
 })
