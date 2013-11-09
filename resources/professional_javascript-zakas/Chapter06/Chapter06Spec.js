7c5e5eb5ed974ef9ebb46ddcab0fca833950334c
Jeff Schmidt <jas@535consulting.com>
2013-11-08 21:33:15 -0700

    Created initial unit test for Chapter 6 to get a feel for Jasmine.

--- resources/professional_javascript-zakas/Chapter06/Chapter06Spec.js
+++ resources/professional_javascript-zakas/Chapter06/Chapter06Spec.js
@@ -1,10 +1,30 @@
 // chapter 6 tests
 
 describe("Chapter 6 Specs - Object Oriented Programming", function(){
-  
-  // we need 1 test to make the Jasmine test suite to run.
-  it("pages 173-216", function() {
-    expect(true).toBe(true);
-  });
-    
+
+    describe("page 174 - basic literal object creation", function(){
+
+        var person = {
+            name: "Nicholas",
+            age: 29,
+            job: "Software Engineer",
+
+            sayName: function() {
+                return this.name;
+            }
+        };
+
+        it("name", function() {
+            expect(person.name).toBe("Nicholas");
+        });
+        it("age", function() {
+            expect(person.age).toBe(29);
+        });
+        it("job", function() {
+            expect(person.job).toBe("Software Engineer");
+        });
+        it("sayName() returns name property value", function() {
+            expect(person.sayName()).toBe("Nicholas");
+        });
+    });
 });
