b1f22ad63ddaa2dbd493b4c800aace98425a170b
Jeff Schmidt <jas@535consulting.com>
2013-11-08 22:11:58 -0700

    Added tests for read-only property. Not sure on correct approach to more generically test for an expected error to be thrown.

--- resources/professional_javascript-zakas/Chapter06/Chapter06Spec.js
+++ resources/professional_javascript-zakas/Chapter06/Chapter06Spec.js
@@ -27,4 +27,44 @@ describe("Chapter 6 Specs - Object Oriented Programming", function(){
             expect(person.sayName()).toBe("Nicholas");
         });
     });
+
+    describe("page 175 - read-only property configuration", function(){
+
+        var person = {};
+        Object.defineProperty(person, "name", {
+            configurable: false,    // read-only
+            value: "Nicholas"       // Initial and only value
+        });
+
+        it("name", function() {
+            expect(person.name).toBe("Nicholas");
+        });
+        person.name = "Nick";
+        it("name change ignored", function() {
+            expect(person.name).toBe("Nicholas");
+        });
+        delete person.name;
+        it("name deletion ignored", function() {
+            expect(person.name).toBe("Nicholas");
+        });
+
+        /*
+         * Once configurable is set to false, it cannot be set to true. Throws error.
+         *
+         * jascks - Well, good idea expecting an error to be thrown, but Chrome, Firefox
+         * and npm (via Node.js) all have different messages in the error, which is
+         * what toThrow() is trying to match.  So, whether the test passes or fails
+         * is environment specific.
+         *
+        it("setting name configurable true throws error", function() {
+            expect(function() {
+
+                Object.defineProperty(person, "name", {
+                    configurable: true, // No can do
+                    value: "Nicholas"
+                });
+            }).toThrow(new TypeError("Cannot redefine property: name"))
+        });
+        */
+    });
 });
